79911c423200eb781478678316805d80d459a7d3
Samujjal Choudhury <35115461+SamujjalChoudhury@users.noreply.github.com>
2019-04-15 14:12:17 +0530

    Create ImprovedEulerMethod.c

---
+++ NMCP/ImprovedEulerMethod.c
@@ -0,0 +1,39 @@
+// TODO: Initialize the program with the function given in the question
+
+#include<stdio.h>
+
+//Function given in the question
+float f(float x, float y)
+{
+	return (/*Function*/);
+}
+
+int main()
+{
+	float x0, y0, x1, y1, h, xn, y11;
+	int i = 0;
+
+	//Input of the data
+	printf("Euler's Method : \nx0 : ");
+	scanf("%f", &x0);	// x0
+	printf("y0 : ");
+	scanf("%f", &y0);	//y0
+	printf("h : ");
+	scanf("%f", &h);	//h
+	printf("Xn : ");
+	scanf("%f", &xn);	//xn
+	
+	printf("\n\n\n");
+	
+	while(x0<xn){
+		y1 = y0 + h*f(x0, y0);
+		x1 = x0 + h;
+		y11 = y0 + (h/2)*(f(x0, y0) + f(x1, y1));
+		x0 = x1;
+		y1 = y11;
+		y0 = y1;
+		printf("When x(%d) = %f\ty(%d) = %f\ty(%d)* = %f\n", i, x0, i, y0, ++i, y11);
+	}
+
+	return 0;
+}
