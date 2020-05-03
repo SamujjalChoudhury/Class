162816e1995406416d58700b6af23fc70ea41c41
Samujjal Choudhury <35115461+SamujjalChoudhury@users.noreply.github.com>
2020-05-04 00:11:48 +0530

    Add files via upload

---
+++ Compiler Design/04/input.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+float a;
+float b;
+float c;
+clrscr();
+printf("Enter the value of a ");
+scanf("%f",&a);
+printf("Enter the value of b");
+scanf("%f",&b);
+c=a+b;
+printf("The sum of two numbers is %f",c);
+scanf("%f",&c);
+}
