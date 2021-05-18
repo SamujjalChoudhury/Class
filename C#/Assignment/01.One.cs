eaad17da8807c640ec40ec6d642c7e6704116b0a
Samujjal Choudhury <35115461+SamujjalChoudhury@users.noreply.github.com>
2021-05-18 16:30:19 +0530

    Add files via upload

---
+++ C#/Assignment/01.One.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+// First class to print "C Sharp"
+class One
+{
+    public void display()
+    {
+        System.Console.Write("C Sharp ");
+        return;
+    }
+}
+
+// Second Class to print "Programming"
+class Two
+{
+    public void display()
+    {
+        System.Console.WriteLine("Programming");
+        return;
+    }
+}
+
+class Program
+    {
+    static void Main(string[] args)
+    {
+        One one = new One();
+        Two two  = new Two();
+            
+        one.display();
+        two.display();
+    }
+}
+
+
+// SamujjalChoudhury
